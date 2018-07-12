63549bf1a8de68b98ab51fe81cc2b468bffef630
Jeroen Desloovere <info@jeroendesloovere.be>
2018-07-12 11:35:49 +0200

    Add missing getItems() method in SitemapProviderInterface

--- src/Provider/SitemapProviderInterface.php
+++ src/Provider/SitemapProviderInterface.php
@@ -3,10 +3,12 @@
 namespace JeroenDesloovere\SitemapBundle\Provider;
 
 use JeroenDesloovere\SitemapBundle\Item\ChangeFrequency;
+use JeroenDesloovere\SitemapBundle\Item\SitemapItems;
 
 interface SitemapProviderInterface
 {
     public function getKey(): string;
+    public function getItems(): SitemapItems;
     public function createItem(string $url, \DateTime $lastModifiedOn, ChangeFrequency $changeFrequency): void;
     public function createItems(): void;
 }
